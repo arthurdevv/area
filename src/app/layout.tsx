1336bccb4304c8b692236f4fc7fc9c3682bad597
Arthur Henrique <arthurhenrique.dev@gmail.com>
2026-05-19 17:42:53 -0300

    feat: enhance layout with font imports and remove unused components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
-import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import type { Metadata } from "next";
-import { Crimson_Text, DM_Sans, Roboto_Mono } from "next/font/google";
+import {
+  Crimson_Text,
+  DM_Sans,
+  Reddit_Mono,
+  Rethink_Sans,
+  Roboto_Mono,
+} from "next/font/google";
 
 import Navigation from "@/components/Navigation";
-import { SmoothScroll } from "@/components/SmoothScroll";
 
 import "./globals.css";
 import "lenis/dist/lenis.css";
@@ -31,24 +35,31 @@ const robotoMono = Roboto_Mono({
   variable: "--font-roboto-mono",
 });
 
+const redditMono = Reddit_Mono({
+  subsets: ["latin"],
+  variable: "--font-reddit-mono",
+});
+
+const rethinkSans = Rethink_Sans({
+  subsets: ["latin"],
+  variable: "--font-rethink-sans",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <html
-        lang="en"
-        className={`${dmSans.variable} ${crimsonText.variable} ${robotoMono.variable} h-full antialiased`}
-      >
-        <body>
-          <div className="relative flex min-h-full max-w-7xl flex-col items-center pb-5">
-            <Navigation />
-            <SmoothScroll>{children}</SmoothScroll>
-          </div>
-        </body>
-      </html>
-      <Analytics />
-      <SpeedInsights />
-    </>
+    <html
+      lang="en"
+      className={`${dmSans.variable} ${crimsonText.variable} ${robotoMono.variable} ${redditMono.variable} ${rethinkSans.variable} h-full antialiased`}
+    >
+      <body>
+        <div className="relative flex min-h-full max-w-7xl flex-col items-center pb-5">
+          <Navigation />
+          {children}
+          <SpeedInsights />
+        </div>
+      </body>
+    </html>
   );
 }
